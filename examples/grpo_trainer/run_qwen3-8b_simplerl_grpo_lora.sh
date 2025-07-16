8977e44bce77a5972afd5db4f92322ecd286b17d
patrlean <tianyou.2001@outlook.com>
2025-07-16 21:31:00 +0000

    edit config

--- examples/grpo_trainer/run_qwen3-8b_simplerl_grpo_lora.sh
+++ examples/grpo_trainer/run_qwen3-8b_simplerl_grpo_lora.sh
@@ -5,9 +5,9 @@ python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files=$HOME/data/simplerl/train.parquet \
     data.val_files=$HOME/data/gsm8k/test.parquet \
-    data.train_batch_size=256 \
+    data.train_batch_size=32 \
     data.max_prompt_length=1024 \
-    data.max_response_length=16384 \
+    data.max_response_length=8192 \
     data.filter_overlong_prompts=True \
     data.truncation='error' \
     data.shuffle=False \
@@ -16,7 +16,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.model.use_shm=True \
     actor_rollout_ref.model.lora_rank=64 \
     actor_rollout_ref.model.lora_alpha=32 \
-    actor_rollout_ref.actor.optim.lr=1e-6 \
+    actor_rollout_ref.actor.optim.lr=5e-7 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=8 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=2 \
@@ -28,7 +28,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.fsdp_config.param_offload=False \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=2 \
-    actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=4 \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
     actor_rollout_ref.rollout.n=4 \
@@ -39,10 +39,11 @@ python3 -m verl.trainer.main_ppo \
     algorithm.use_kl_in_reward=False \
     trainer.critic_warmup=0 \
     trainer.logger='["console","wandb"]' \
+    trainer.val_before_train=False \
     trainer.project_name='qwen3-8b-grpo' \
     trainer.experiment_name='qwen3-8b_simplerl_grpo_lora' \
     trainer.n_gpus_per_node=8 \
     trainer.nnodes=1 \
-    trainer.save_freq=5 \
+    trainer.save_freq=20 \
     trainer.test_freq=5 \
-    trainer.total_epochs=10 2>&1 | tee verl_demo.log
+    trainer.total_epochs=3 2>&1 | tee verl_demo.log
